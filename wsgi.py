be6d028b2219a61cc337b005f983e49b7b110553
vvcbvx <hythmghnam32@gmail.com>
2025-10-20 09:32:05 +0300

    Fix port configuration in wsgi.py
    Removed environment variable for port and set it to 10000.

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,5 @@
-
 # wsgi.py
-import os
-import logging
 from bot import app
 
 if __name__ == "__main__":
-    # هذا الملف مخصص لـ Render فقط
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=10000)
